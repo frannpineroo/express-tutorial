0d34f0fc1ad57f35ab7cc65a4954ca7529bb9471
franpi <frannpinero@hotmail.com>
2025-12-30 18:01:29 -0300

    conectando frontend con el servidor de express

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
+}))
 
 app.get('/', (req, res) => {
     res.send('Hola desde Express!');
@@ -32,6 +37,10 @@ app.get('/products/:id', (req, res) => {
 
     const requestedProduct = products.find((product) => product.id === id);
     res.json(requestedProduct);
+});
+
+app.get('/message', (req, res) => {
+    res.json({ message: 'Hola desde Express backend' });
 });
 
 app.listen(3000, () => {
