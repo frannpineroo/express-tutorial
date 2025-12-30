b3e1e71037ae3f8f4607387b5c4e89dd58e8da0d
franpi <frannpinero@hotmail.com>
2025-12-30 17:41:48 -0300

    manejo de parametros de ruta

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ app.get('/products', (req, res) => {
     ])
 });
 
+app.get('/products/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const products = [
+        {id: 1, name: 'Computadora', price: 800},
+        {id: 2, name: 'Teléfono', price: 500},
+        {id: 3, name: 'Tablet', price: 300}
+    ]
+
+    const requestedProduct = products.find((product) => product.id === id);
+    res.json(requestedProduct);
+});
+
 app.listen(3000, () => {
     console.log('El servidor está corriendo en el puerto 3000');
 });
